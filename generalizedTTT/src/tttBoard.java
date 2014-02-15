60cd8a97bfb3fef94bd2c3fa697c6d2f17e8eed9
jennyxing27 <jennyxing27@gmail.com>
2014-02-15 13:32:17 -0500

    2/3 done with loadPerfectSquare
    now works for both odd and double even sizes.

--- generalizedTTT/src/tttBoard.java
+++ generalizedTTT/src/tttBoard.java
@@ -17,7 +17,7 @@ public class tttBoard {
 	TreeSet<Integer> player1Spots;
 
 	public static void main(String args[]) {
-		// needed main to run for testing purposes
+		tttBoard testBoard = new tttBoard(4, "x", "o");
 	}
 
 	public tttBoard(Integer size, String p0, String p1) {
@@ -34,50 +34,94 @@ public class tttBoard {
 	private void loadPerfectSquare() {
 
 		if (size < 3)
-			throw new RuntimeException(
-					"magic squares of size less than 3 cannot be generated.");
+			throw new RuntimeException("invalid size for tic-tac-toe board.");
 
+		// generation for odd sizes
 		if (size % 2 == 1) {
 			for (int row = 1; row <= size; row++) {
 				for (int col = 1; col <= size; col++) {
+					// formula given by wikipedia
 					int num = size * ((row + col - 1 + size / 2) % size)
 							+ ((row + 2 * col - 2) % size) + 1;
-					perfectSquare.put(getKey(col - 1, row - 1), num);
-					// System.out.println("(" + (row - 1) + ", " + (col - 1)
-					// + "): " + num);
+					perfectSquare.put(getKey(row - 1, col - 1), num);
 				}
-
 			}
+			// printMagicSquare();
 		}
-		// TODO implement for even sizes
+
+		// generation for double even (divisible by 4) sizes. see
+		// http://www.1728.org/magicsq2.htm
+		if (size % 4 == 0) {
+
+			// fills the "diagonals"
+			int count = 1;
+			final int smaller_size = size / 4;
+			for (int row = 1; row <= size; row++) {
+				for (int col = 1; col <= size; col++) {
+					int key = getKey(row - 1, col - 1);
+					// fills in the corner squares that are size/4 x size/4
+					if ((col <= smaller_size && row <= smaller_size)
+							|| (col > size - smaller_size && row > size
+									- smaller_size)
+							|| (col <= smaller_size && row > size
+									- smaller_size)
+							|| (col > size - smaller_size && row <= smaller_size)) {
+						perfectSquare.put(key, count);
+					}
+
+					// fills the inner square that is size/2 x size/2
+					if (col > smaller_size && col <= size - smaller_size
+							&& row > smaller_size && row <= size - smaller_size) {
+						perfectSquare.put(key, count);
+					}
+
+					count++;
+				}
+			}
+
+			// fills in the rest from the bottom right to top left
+			count = 1;
+			for (int row = size - 1; row >= 0; row--) {
+				for (int col = size - 1; col >= 0; col--) {
+					int key = getKey(row, col);
+					if (!perfectSquare.containsKey(key)) {
+						perfectSquare.put(key, count);
+					}
+					count++;
+				}
+			}
+			// printMagicSquare();
+		}
+
+		// TODO implement for single even sizes http://www.1728.org/magicsq3.htm
 	}
 
-	// params x(column) and y(row): coordinates within the square (0 to
+	// params row and col: coordinates within the square (0 to
 	// size^2-1)
 	// return int key corresponding to the key in the maps for those coords
-	private int getKey(int x, int y) {
-		return x + size * y;
+	private int getKey(int row, int col) {
+		return size * row + col;
 	}
 
 	// param player: 0 or 1, the player number
-	// params x and y: coordinates within the square
+	// params row and col: coordinates within the square
 	// return boolean true if move successful, false otherwise
-	private boolean move(int player, int x, int y) {
-		if (boardMap.containsKey(getKey(x, y))) { // already occupied
+	private boolean move(int player, int row, int col) {
+		if (boardMap.containsKey(getKey(row, col))) { // already occupied
 			return false;
 		}
 		if (player != 0 && player != 1) { // invalid player
 			return false;
 		}
-		if (x >= size || y >= size) { // invalid coords
+		if (row >= size || col >= size) { // invalid coords
 			return false;
 		}
 		// valid move
-		boardMap.put(getKey(x, y), player == 1 ? player1 : player0);
+		boardMap.put(getKey(row, col), player == 1 ? player1 : player0);
 		if (player == 1) {
-			player1Spots.add(perfectSquare.get(getKey(x, y)));
+			player1Spots.add(perfectSquare.get(getKey(row, col)));
 		} else {
-			player0Spots.add(perfectSquare.get(getKey(x, y)));
+			player0Spots.add(perfectSquare.get(getKey(row, col)));
 		}
 		return true;
 	}
@@ -158,5 +202,30 @@ public class tttBoard {
 	// perform the winning operation given the winner
 	private void win(String winner) {
 
+	}
+
+	// prints magic square in row-major oder
+	private void printMagicSquare() {
+		System.out.println("start");
+		for (int row = 0; row < size; row++) {
+			for (int col = 0; col < size; col++) {
+				// System.out.println("(" + row + ", " + col + "): "
+				// + perfectSquare.get(getKey(row, col)));
+				if (perfectSquare.containsKey(getKey(row, col))) {
+					int value = perfectSquare.get(getKey(row, col));
+					System.out.print(value);
+					if (value < 10000)
+						System.out.print(" ");
+					if (value < 1000)
+						System.out.print(" ");
+					if (value < 100)
+						System.out.print(" ");
+					if (value < 10)
+						System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+		System.out.println("end");
 	}
 }
