1dc4ee3b4e2fa569057f5417139e27821ba19635
jennyxing27 <jennyxing27@gmail.com>
2014-02-23 22:02:50 -0500

    added excel sheet
    documents general formulas for magic cubes. incomplete. how many ways
    are there to win for dimensions higher than 3?

--- generalizedTTT/src/genTTTBoard.java
+++ generalizedTTT/src/genTTTBoard.java
@@ -21,7 +21,7 @@ public class genTTTBoard {
 	private TreeSet<Integer> openSpots;
 	private TreeSet<Integer> player0Pairs;
 	private TreeSet<Integer> player1Pairs;
-	
+
 	final int MAX_SPACES = 1000000;
 
 	public static void main(String args[]) {
@@ -31,11 +31,11 @@ public class genTTTBoard {
 	public genTTTBoard(Integer size, Integer dim, String p0, String p1) {
 		this.size = size;
 		dimension = dim;
-		magicNum = (size * (pow(size,dimension) + 1)) / 2;
+		magicNum = (size * (pow(size, dimension) + 1)) / 2;
 		player0 = p0;
 		player1 = p1;
-		boardMap = new HashMap<Integer, String>(pow(size,dim), (float) 1.0);
-		magicCube = new HashMap<Integer, Integer>(pow(size,dim), (float) 1.0);
+		boardMap = new HashMap<Integer, String>(pow(size, dim), (float) 1.0);
+		magicCube = new HashMap<Integer, Integer>(pow(size, dim), (float) 1.0);
 		loadMagicCube();
 		player0Spots = new TreeSet<Integer>();
 		player1Spots = new TreeSet<Integer>();
@@ -44,33 +44,40 @@ public class genTTTBoard {
 		player0Pairs = new TreeSet<Integer>();
 		player1Pairs = new TreeSet<Integer>();
 	}
-	
-	public int getSize() { return size; }
-	
-	public HashMap<Integer, String> getBoardMap() { return boardMap; }
-	
-	public int getDimension() { return dimension; }
-	
-	//simple method to return x^exp for ints
+
+	public int getSize() {
+		return size;
+	}
+
+	public HashMap<Integer, String> getBoardMap() {
+		return boardMap;
+	}
+
+	public int getDimension() {
+		return dimension;
+	}
+
+	// simple method to return x^exp for ints
 	private int pow(int x, int exp) {
 		int retval = 1;
 		while (true) {
-			if (exp == 0) break;
+			if (exp == 0)
+				break;
 			retval *= x;
 			exp--;
 		}
 		return retval;
 	}
-	
-	//load all possible key values into openSpots set
+
+	// load all possible key values into openSpots set
 	private void loadOpenSpots() {
-		for (int i=0; i<pow(size, dimension); i++) {
+		for (int i = 0; i < pow(size, dimension); i++) {
 			openSpots.add(i);
 		}
 	}
 
 	// load Integer values into the correct values in the magicSquare map
-	//TODO: implement for n-dimensions
+	// TODO: implement for n-dimensions
 	private void loadMagicCube() {
 
 	}
@@ -79,8 +86,8 @@ public class genTTTBoard {
 	// return int key corresponding to the key in the maps for those coords
 	private int getKey(int[] coords) {
 		int retval = 0;
-		for (int i=0; i<dimension; i++) {
-			retval += pow(size,i)*coords[i];
+		for (int i = 0; i < dimension; i++) {
+			retval += pow(size, i) * coords[i];
 		}
 		return retval;
 	}
@@ -89,52 +96,52 @@ public class genTTTBoard {
 	// param coords: coordinates within the n-cube
 	// return boolean true if move successful, false otherwise
 	private boolean move(int player, int[] coords) {
-//TODO
+		// TODO
 		return false;
 	}
-	
-	//update pairs set for player to include all new possible pairs
-	//if the last move was a winning one, call win() method
-	//NOTE: the current move has NOT been added to the spots set
+
+	// update pairs set for player to include all new possible pairs
+	// if the last move was a winning one, call win() method
+	// NOTE: the current move has NOT been added to the spots set
 	private boolean updatePairs(int key, int player) {
-//TODO
+		// TODO
 		return false;
 	}
 
 	// check for win or draw conditions
 	// update the relevant player pairs set to include pairs wit
-	//NOTE: the current move HAS been added to relevant pairs set
-	//param player: 0 or 1, the player number
+	// NOTE: the current move HAS been added to relevant pairs set
+	// param player: 0 or 1, the player number
 	private boolean check(int player) {
 		if (player != 0 && player != 1) { // invalid player
 			return false;
 		}
-		//checkWin() // deprecated
+		// checkWin() // deprecated
 		TreeSet<Integer> pairs = (player == 0) ? player0Pairs : player1Pairs;
-		String winner = (player == 0) ? player0 : player1; //not necessarily the winner yet
-		if (pairs.contains(magicNum)) { //should be redundant
+		String winner = (player == 0) ? player0 : player1; // not necessarily
+															// the winner yet
+		if (pairs.contains(magicNum)) { // should be redundant
 			win(winner);
 			return true;
 		}
 		checkDraw();
 		return true;
 	}
-	
-	
-	//method to check for draw condition
-	//TODO: currently only checks that all spaces are filled
-	//TODO: draw condition can occur before this is true
+
+	// method to check for draw condition
+	// TODO: currently only checks that all spaces are filled
+	// TODO: draw condition can occur before this is true
 	private void checkDraw() {
 		if (openSpots.size() == 0)
 			draw();
 	}
-	
-	//perform draw operation
+
+	// perform draw operation
 	private void draw() {
-		
+
 	}
 
-	//sum the array of size elements
+	// sum the array of size elements
 	private int sum(int[] nPair) {
 		int retval = 0;
 		for (int i = 0; i < size; i++) {
@@ -155,6 +162,5 @@ public class genTTTBoard {
 		magicCube.put(x, yval);
 		magicCube.put(y, xval);
 	}
-	
-	
+
 }
